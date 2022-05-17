6b2581f6abcb840c18b930ef8184be757e293a8f
Meir Ben-Shimon <Meir.b@qualisystems.com>
2022-05-17 17:08:54 +0300

    prevent customization

--- vm-in-vlan-from-template/main.tf
+++ vm-in-vlan-from-template/main.tf
@@ -54,16 +54,5 @@ resource "vsphere_virtual_machine" "vm" {
 
  clone {
    template_uuid = data.vsphere_virtual_machine.template.id
-   customize {
-      linux_options {
-        host_name = "hello-world"
-        domain    = "example.com"
-      }
-      network_interface {
-        ipv4_address = "172.16.0.10"
-        ipv4_netmask = 24
-      }
-      ipv4_gateway = "10.0.0.1"
-    }
   }
 }
